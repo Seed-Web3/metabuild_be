860dd8cdaebbd9ce91e3fb7400cad4be861965d4
Csaba Soti <sotcsa@gmail.com>
2022-11-07 07:02:33 +0100

    BadgeEndpoint

--- src/main/java/com/seed/careerhub/endpoint/BadgeEndpoint.java
+++ src/main/java/com/seed/careerhub/endpoint/BadgeEndpoint.java
@@ -1,6 +1,11 @@
 package com.seed.careerhub.endpoint;
 
 import com.seed.careerhub.jpa.BadgeRepository;
+import com.seed.careerhub.model.BadgeRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,10 @@ public class BadgeEndpoint {
         this.badgeRepository = badgeRepository;
     }
 
+    @Operation(summary = "Authenticates a user via signed message with MetaMask")
+    @PostMapping("setCategory")
+    public ResponseEntity<?> setCategory(@RequestBody BadgeRequest badgeRequest) {
+        return ResponseEntity.ok("OK");
+    }
 
 }
---
+++ src/main/java/com/seed/careerhub/model/BadgeRequest.java
@@ -0,0 +1,9 @@
+package com.seed.careerhub.model;
+
+import lombok.Data;
+
+@Data
+public class BadgeRequest {
+    String category;
+//    List<Badge> badges;
+}
